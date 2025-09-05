ad1e84355224dcc0f1b4cfe4a012815bac825454
vishakhash2001 <vishakha.sharma@whizlabs.com>
2025-09-05 19:50:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-    return a / b
-
-def divide_numbers(a, b):
-    return a + b 
+def divide(a, b): 
+return a / b 
+if __name__ == "__main__": 
+print("Addition result: ", add(8, 2)) 
